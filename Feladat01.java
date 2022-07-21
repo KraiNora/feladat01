f109b1897e9b3a0b03e2295eda8898eafe1088a7
KraiNora <90151535+KraiNora@users.noreply.github.com>
2022-07-21 20:33:04 +0200

    Update Feladat01.java

--- Feladat01.java
+++ Feladat01.java
@@ -1,30 +1,20 @@
 public class Feladat01 {
 
-    public int feladat01(String[] szinek){
+    public int feladat01(String[] szinek) {
         int len = szinek.length;
-        if(len<=1)
+        if (len <= 1) {
             return 0;
-
-        int result = 0;
-
-        int k = 2;
-
-        for(int i=0;i<len;i++){
-            result += factorial(len)/(factorial(k) * factorial(len-k));
-            k++;
         }
 
-        return result;
+        return pow(len) - len - 1;
     }
 
-    static int factorial(int end){
-        int fact = 1;
-        int i = 1;
-        while(i<=end){
-            fact*=i;
-            i++;
+    static int pow(int n) {
+        int pow = 1;
+        for (int i = 1; i <= n; i++) {
+            pow *= 2;
         }
-        return fact;
+        return pow;
     }
 
 
@@ -34,7 +24,7 @@ public class Feladat01 {
         String[] szinek0 = {};
         String[] szinek1 = {"piros"};
         String[] szinek2 = {"piros", "kek"};
-        String[] szinek3 = {"piros", "kek", "zold" };
+        String[] szinek3 = {"piros", "kek", "zold"};
         int keveres = f.feladat01(szinek3);
         System.out.println("valasz: " + keveres);
     }
